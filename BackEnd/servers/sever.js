786da992b3f1bed5379752fc29288a80dfe94c52
sajal verma <mv912293@gmail.com>
2025-11-30 23:27:48 +0530

    30-11-2025

--- BackEnd/servers/sever.js
+++ BackEnd/servers/sever.js
@@ -1,32 +1,44 @@
 import express from "express";
 import dotenv from "dotenv";
+import http from "http";   // <-- REQUIRED
 import connectDB from "../database/connect.js";
 import userRout from "../routers/userRout.js";
 import requestRout from "../routers/requestRout.js";
-import QuestionRout from "../routers/QuestionRout.js"
+import QuestionRout from "../routers/QuestionRout.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import socketConnection from "../controller/socket.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Connect DB
 connectDB();
 
+// Middlewares
 app.use(cookieParser());
-// middleware
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
-app.use(express.json())
+app.use(express.json());
 
+// Routes
 app.use("/user", userRout);
 app.use("/request", requestRout);
-app.use("/Question",QuestionRout);
+app.use("/Question", QuestionRout);
 
-// create server
-app.listen(port, () => {
+// ------------------------------------------------------
+// ❗ CREATE HTTP SERVER
+const server = http.createServer(app);
+
+// ❗ PASS HTTP SERVER TO SOCKET.IO
+socketConnection(server);
+// ------------------------------------------------------
+
+// Start Server
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
